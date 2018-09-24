31a6496700e758ade0919ab89d8ade3c61e981c3
karthik.ms <karthik.ms@zohocorp.com>
2018-09-24 14:50:52 +0530

    Created TransferAmountViewController

---
+++ Cost Track/Cost Track/Controllers/ViewControllers/TransferAmountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TransferAmountViewController.swift
+//  Cost Track
+//
+//  Created by Karthik M S on 22/09/18.
+//  Copyright © 2018 Karthik M S. All rights reserved.
+//
+
+import UIKit
+
+class TransferAmountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		view.backgroundColor = .blue
+    }
+
+}
